ccf60871493f340755f7c1d4bee82e486c40dcdd
tommy_pi <w98935977@gmail.com>
2025-05-25 00:44:31 +0000

    feat: add Prometheus monitoring with Node Exporter and update backend metrics endpoint

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,15 @@ const { Client } = require('pg');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const client = require('prom-client');
+const register = client.register;
+
+client.collectDefaultMetrics();
+
+// 一個簡單的 GET API
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
 
 app.get('/db', async (req, res) => {
   const client = new Client({ connectionString: process.env.DATABASE_URL });
@@ -18,11 +27,14 @@ app.get('/db', async (req, res) => {
   }
 });
 
-// 一個簡單的 GET API
-app.get('/', (req, res) => {
-  res.send('Hello from backend!');
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
 
 app.listen(port, () => {
   console.log(`Backend listening on port ${port}`);
 });
+
+
+
